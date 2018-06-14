f769da6fb2631310b7d5ce20e4e08ef31ad125ff
Ivonei Junior <ivoneijr@gmail.com>
2018-06-13 21:48:36 -0300

    add Message to components file level.

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import * as actions from '../../reducers/chat';
-import Message from './Message/index.jsx';
+import Message from '../Message/index.jsx';
 import { gamoraMessages, hulkShapeMessage } from '../../utils/messages';
 import CHAT_SHAPE from './shape';
 
